24212c76040de4718d8fd49eb7fefdf9ff52b5c0
KP-Bee <apivat.janhom@kingpower.com>
2020-10-14 13:38:23 +0700

    customer take

--- src/views/pickup/Issue/CustomerTake.vue
+++ src/views/pickup/Issue/CustomerTake.vue
@@ -5,63 +5,44 @@
         <CCard>
           <CCardHeader>
             <strong>Information </strong>
-            <div class="card-header-actions">
-              <a
-                href="https://coreui.io/vue/docs/components/form-components"
-                class="card-header-action"
-                rel="noreferrer noopener"
-                target="_blank"
-              >
-                <small class="text-muted">docs</small>
-              </a>
-            </div>
           </CCardHeader>
           <CCardBody>
             <CRow>
-              <CCol sm="12">
-                <CInput label="Name" placeholder="Enter your name" />
-              </CCol>
-            </CRow>
-            <CRow>
-              <CCol sm="12">
+              <CCol sm="4">
                 <CInput
-                  label="Credit Card Number"
-                  placeholder="0000 0000 0000 0000"
+                  label="Shopping card / Pack no. / Member Card"
+                  placeholder="enter your card number"
+                />
+              </CCol>
+              <CCol sm="2">
+                <CInput label="Order no." placeholder="enter your order no" />
+              </CCol>
+              <CCol sm="6">
+                <CInput
+                  label="Passport no."
+                  placeholder="enter your passport no"
                 />
               </CCol>
             </CRow>
             <CRow>
-              <CCol sm="4">
-                <CSelect
-                  label="Month"
-                  :options="[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]"
-                />
+              <CCol sm="2">
+                <CInput label="Customer's name" readonly="true" />
               </CCol>
-              <CCol sm="4">
-                <CSelect
-                  label="Year"
-                  :options="[
-                    2014,
-                    2015,
-                    2016,
-                    2017,
-                    2018,
-                    2019,
-                    2020,
-                    2021,
-                    2022,
-                    2023,
-                    2024,
-                    2025,
-                  ]"
-                />
+              <CCol sm="8">
+                <CInput label="Recipient information detail" readonly="true" />
               </CCol>
-              <CCol sm="4">
-                <CInput label="CVV/CVC" placeholder="123" />
+              <CCol sm="2">
+                <CButton block color="primary" class="mt-4">Recipient information (F8)</CButton>
               </CCol>
             </CRow>
           </CCardBody>
         </CCard>
+      </CCol>
+      <CCol sm="12">
+        <CCardHeader>
+          <strong>Packing Detail </strong>
+        </CCardHeader>
+        <CCardBody> </CCardBody>
       </CCol>
     </CRow>
   </div>
